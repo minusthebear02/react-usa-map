008f49c0886ba0712e762ff44f6c36bc0d1a7bae
Mark Froehlich <mfroeh2@gmail.com>
2020-08-21 15:30:08 -0400

    fix

--- src/components/USAState.js
+++ src/components/USAState.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const USAState = ({fill, dimensions, state, onClickState, stateName, customAttrs}) => {
+const USAState = ({fill, dimensions, state, onClickState, stateName, ...customAttrs}) => {
   return (
     <path d={dimensions} fill={fill} data-name={state} className={`${state} state`} onClick={onClickState} {...customAttrs}>
       <title>{stateName}</title>
