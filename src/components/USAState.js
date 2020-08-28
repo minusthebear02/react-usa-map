bbb2abb879bc57bb84a62535f91e170236ef9dfc
Mark Froehlich <mfroeh2@gmail.com>
2020-08-28 14:35:41 -0400

    test

--- src/components/USAState.js
+++ src/components/USAState.js
@@ -3,7 +3,7 @@ import React from "react";
 const USAState = ({fill, dimensions, state, onClickState, stateName, ...customAttrs}) => {
   return (
     <>
-      <path d={dimensions} fill={fill} data-name={state} className={`${state} state`} onClick={onClickState} {...customAttrs}>
+      <path d={dimensions} fill={fill} data-name={state} data-state="yerp" className={`${state} state`} onClick={onClickState} {...customAttrs}>
         <title>{stateName}</title>
       </path>
       <text>{stateName}</text>
