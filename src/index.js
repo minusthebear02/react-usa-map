5b0900c5aebdad4e7b87aaf3d7354c90280d4c7e
Mark Froehlich <mfroeh2@gmail.com>
2020-09-02 13:50:24 -0400

    added id to svg

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ class USAMap extends React.Component {
   render() {
     return (
       <svg
+        id="us-state-map"
         className="us-state-map"
         xmlns="http://www.w3.org/2000/svg"
         width={this.props.width}
